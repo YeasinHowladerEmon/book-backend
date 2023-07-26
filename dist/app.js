d857613ae8697d6c63fa86a439b1fbd32a35b9dd
emonibnsalim <emonibnsali@gmail.com>
2023-07-27 00:04:59 +0600

    little bit changes

--- dist/app.js
+++ dist/app.js
@@ -8,6 +8,7 @@ const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const globalErrorHanlder_1 = __importDefault(require("./app/middleware/globalErrorHanlder"));
 const routes_1 = __importDefault(require("./app/routes"));
+const http_status_1 = __importDefault(require("http-status"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use((0, cookie_parser_1.default)());
@@ -19,4 +20,17 @@ app.use("/api/v1/", routes_1.default);
 // token save req.user
 // global error hanlder
 app.use(globalErrorHanlder_1.default);
+app.use((req, res, next) => {
+    res.status(http_status_1.default.NOT_FOUND).json({
+        success: false,
+        message: 'Not Found',
+        errorMessages: [
+            {
+                path: req.originalUrl,
+                message: 'API not found'
+            }
+        ]
+    });
+    next();
+});
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import globalErrorHandler from './app/middleware/globalErrorHanlder';
 import router from './app/routes';
+import httpStatus from 'http-status';
 
 const app: Application = express();
 
@@ -25,5 +26,19 @@ app.use("/api/v1/", router)
 
 app.use(globalErrorHandler)
 
+
+app.use((req: Request, res:Response, next: NextFunction) => {
+    res.status(httpStatus.NOT_FOUND).json({
+        success: false,
+        message: 'Not Found',
+        errorMessages: [
+            {
+                path: req.originalUrl,
+                message: 'API not found'
+            }
+        ]
+    });
+    next()
+})
 
 export default app;
